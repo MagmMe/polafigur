774f3d2704d6a696a7b548e7ea55cd00685f6479
MagmMe <marcin@magm.me>
2020-07-16 14:56:49 +0200

    Initial Commit

---
+++ Obliczenia pola figur geometrycznych/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Obliczenia pola figur geometrycznych
+//
+//  Created by Marcin Magiera on 16/07/2020.
+//  Copyright © 2020 Magme Agency. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
